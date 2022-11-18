ef47b1c179e72c191a27931a22652f241106e8b6
WikiOur <69310570+wikiour@users.noreply.github.com>
2022-11-18 13:20:55 +0200

    Update mobile-menu.js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,17 @@
 function burger() {
-  let menuBtn = document.querySelector('.burger-open');
+  let menuBtn = document.querySelector('.burger');
   let menu = document.querySelector('.menu-mobile');
   let menuBody = document.querySelector('body');
-  let menuCancel = document.querySelector('.burger-cancel');
+  let menuBtnImgOpen = document.querySelector('.img-open-menu');
+  let menuBtnImgCancel = document.querySelector('.img-close-menu');
 
   menuBtn.addEventListener('click', function () {
     menuBtn.classList.toggle('open');
     menu.classList.toggle('open');
     menuBody.classList.toggle('lock-menu');
-    menuCancel.classList.toggle('cancel');
+    menuBtnImgOpen.classList.toggle('open');
+    menuBtnImgCancel.classList.toggle('close');
+
   });
 }
 
